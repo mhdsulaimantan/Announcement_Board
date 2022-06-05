fc20bf0fb54434a489540fdf50a4a36da443b344
mohammad <mohammad.sulaiman.tanbari@gmail.com>
2022-06-05 11:55:11 +0200

    improve the layout of the page and how to use the passed data

--- Announcements/views.py
+++ Announcements/views.py
@@ -1,13 +1,11 @@
 from datetime import datetime, timedelta
 
+from django.http import HttpResponseBadRequest
 from django.shortcuts import render
-from django.utils.datastructures import MultiValueDictKeyError
 
 """ Announcements dictionary
 keys -> announcement ID || values -> [announcement text , announcement date] 
 """
-# announcements = {}
-# test cases
 announcements = {0: ["Hi, I have a question. Where i could find dancing courses?", datetime.now() - timedelta(days=50)],
                  1: ["Good Morning :), Anyone up to do some hiking on the weekend?",
                      datetime.now() - timedelta(days=44)],
@@ -23,73 +21,33 @@ announcements = {0: ["Hi, I have a question. Where i could find dancing courses?
                  }
 
 
-# View Announcements page
 def Announcement_Page(request, *args, **kwargs):
-    # default value for period is 'All', used to focus on the period button (javascript use)
+    # default value for period (date filter)
     period = "all"
 
-    # when the request method is POST
-    if request.method == "POST":
-        # to use the POST method on the same page
-        # if there is a POST request passing a delete statement then it is a delete POST request
-        try:
-            if request.POST["Delete"]:
-                # delete specific announcement
-                Delete_Announcement(request)
-
-        # if there is a POST request without passing a delete statement then it is a create POST request
-        except MultiValueDictKeyError:
-            # create new announcement
-            Create_Announcement(request)
-
-    # passing all the announcements data to the page
-    announcements_list = []
-
-    # when the request method is GET
     try:
-        # passing a specific period with GET request
-        period_req = request.GET["period"]
-        # sort announcements by key (reverse) from the newest announcement to the oldest
-        for key, value in sorted(announcements.items(), reverse=True):
-            if period_req == "All":
-                announcements_list.append({"id": key,
-                                           "text": value[0],
-                                           "date": value[1]})
+        if request.method == "POST":
+            period = request.POST["period"]
+            # if there is a POST request passing a delete statement
+            if request.POST.get("delete"):
+                delete_announcement(request)
+            # if there is a POST request passing a create statement
+            elif request.POST.get("create"):
+                create_announcement(request)
 
-            elif period_req == "Today":
-                period = "today"
-                if value[1] >= datetime.now() - timedelta(hours=24):
-                    announcements_list.append({"id": key,
-                                               "text": value[0],
-                                               "date": value[1]})
+        elif request.method == "GET":
+            # passing a specific period with GET request
+            if request.GET.get("period"):
+                period = request.GET["period"]
 
-            elif period_req == "Last 7 Days":
-                period = "week"
-                if value[1] >= datetime.now() - timedelta(days=7):
-                    announcements_list.append({"id": key,
-                                               "text": value[0],
-                                               "date": value[1]})
+    except HttpResponseBadRequest as err:
+        raise err
 
-            elif period_req == "Last 30 Days":
-                period = "month"
-                if value[1] >= datetime.now() - timedelta(days=30):
-                    announcements_list.append({"id": key,
-                                               "text": value[0],
-                                               "date": value[1]})
-
-    # when there is no period data passed (default GET request)
-    except MultiValueDictKeyError:
-        for key, value in sorted(announcements.items(), reverse=True):
-            announcements_list.append({"id": key,
-                                       "text": value[0],
-                                       "date": value[1]})
-
-    return render(request, "index.html", {"announcements": announcements_list, "period": period})
+    return render(request, "index.html", {"announcements": show_based_on_period(period), "period": period})
 
 
-# create new announcement
-def Create_Announcement(request, *args, **kwargs):
-    # test if there are any announcements before
+def create_announcement(request, *args, **kwargs):
+    # exist announcements
     if announcements:
         # next announcement id -> get last announcement id and add 1
         announcement_id = list(announcements.keys())[-1] + 1
@@ -104,6 +62,35 @@ def Create_Announcement(request, *args, **kwargs):
     announcements[announcement_id] = [request.POST["announcement_text"], current_date]
 
 
-# delete specific announcement that is passing announcement ID
-def Delete_Announcement(request, *args, **kwargs):
+def delete_announcement(request, *args, **kwargs):
     announcements.pop(int(request.POST["announcement_id"]))
+
+
+def show_based_on_period(period):
+    announcements_list = []
+    # sort announcements by key (reverse) from the newest announcement to the oldest
+    for key, value in sorted(announcements.items(), reverse=True):
+        if period == "all":
+            announcements_list.append({"id": key,
+                                       "text": value[0],
+                                       "date": value[1]})
+
+        elif period == "today":
+            if value[1] >= datetime.now() - timedelta(hours=24):
+                announcements_list.append({"id": key,
+                                           "text": value[0],
+                                           "date": value[1]})
+
+        elif period == "week":
+            if value[1] >= datetime.now() - timedelta(days=7):
+                announcements_list.append({"id": key,
+                                           "text": value[0],
+                                           "date": value[1]})
+
+        elif period == "month":
+            if value[1] >= datetime.now() - timedelta(days=30):
+                announcements_list.append({"id": key,
+                                           "text": value[0],
+                                           "date": value[1]})
+
+    return announcements_list
